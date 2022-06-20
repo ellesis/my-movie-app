8906a966b2c332a20e35d8120fb219f12f7e76e7
Lisa Han <ellesis@gmail.com>
2022-06-19 18:27:10 -0700

    Add Header component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -8,6 +9,8 @@ export default function Home() {
         <meta name="description" content="Lisa's Next Movie App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
     </div>
   )
 }
